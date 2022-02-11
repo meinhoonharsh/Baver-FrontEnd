04e49ef12489672629b4bd66e1e0298f767cacc4
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-12 02:36:18 +0530

    Created HomeCTA Component

---
+++ src/components/HomeCTA.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+export default function HomeCTA() {
+  return (
+    <>
+      <section
+        className="elementor-element elementor-element-fa26c25 elementor-section-stretched elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+        data-id="fa26c25"
+        data-element_type="section"
+        data-settings='{"stretch_section":"section-stretched","background_background":"classic"}'
+      >
+        <div className="elementor-background-overlay" />
+        <div className="elementor-container elementor-column-gap-extended">
+          <div className="elementor-row">
+            <div
+              className="elementor-element elementor-element-2c1087a elementor-column elementor-col-50 elementor-top-column"
+              data-id="2c1087a"
+              data-element_type="column"
+              data-settings='{"background_background":"classic"}'
+            >
+              <div className="elementor-column-wrap  elementor-element-populated">
+                <div className="elementor-widget-wrap">
+                  <div
+                    className="elementor-element elementor-element-dce4cea elementor-widget elementor-widget-heading"
+                    data-id="dce4cea"
+                    data-element_type="widget"
+                    data-widget_type="heading.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <h2 className="elementor-heading-title elementor-size-default">
+                        One of most popular place to submit your list
+                      </h2>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-d4f69a5 elementor-widget elementor-widget-spacer"
+                    data-id="d4f69a5"
+                    data-element_type="widget"
+                    data-widget_type="spacer.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-spacer">
+                        <div className="elementor-spacer-inner" />
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-8908108 elementor-widget elementor-widget-text-editor"
+                    data-id={8908108}
+                    data-element_type="widget"
+                    data-widget_type="text-editor.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-text-editor elementor-clearfix">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor incididunt ut labore et
+                          dolore magna aliqua. Ut enim ad minim veniam
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-9e08d60 elementor-widget elementor-widget-spacer"
+                    data-id="9e08d60"
+                    data-element_type="widget"
+                    data-widget_type="spacer.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-spacer">
+                        <div className="elementor-spacer-inner" />
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-d269ee1 elementor-widget elementor-widget-button"
+                    data-id="d269ee1"
+                    data-element_type="widget"
+                    data-widget_type="button.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-button-wrapper">
+                        <a
+                          href="#"
+                          className="elementor-button-link elementor-button elementor-size-sm"
+                          role="button"
+                        >
+                          <span className="elementor-button-content-wrapper">
+                            <span className="elementor-button-text">
+                              Get Started Today
+                            </span>
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              className="elementor-element elementor-element-fa732f9 elementor-hidden-tablet elementor-hidden-phone elementor-column elementor-col-50 elementor-top-column"
+              data-id="fa732f9"
+              data-element_type="column"
+            >
+              <div className="elementor-column-wrap">
+                <div className="elementor-widget-wrap"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
