49601a5c95db5c540d771f5c5c95a60889c28ab1
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-12 02:23:13 +0530

    Created Trending HoRecas Component

---
+++ src/components/TrendingHorecas.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function TrendingHorecas() {
+  return (
+    <>
+      <section
+        className="elementor-element elementor-element-18d1396 elementor-section-stretched elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-top-section"
+        data-id="18d1396"
+        data-element_type="section"
+        data-settings='{"stretch_section":"section-stretched","background_background":"classic"}'
+      >
+        <div className="elementor-container elementor-column-gap-extended">
+          <div className="elementor-row">
+            <div
+              className="elementor-element elementor-element-54a4f64 elementor-column elementor-col-100 elementor-top-column"
+              data-id="54a4f64"
+              data-element_type="column"
+            >
+              <div className="elementor-column-wrap  elementor-element-populated">
+                <div className="elementor-widget-wrap">
+                  <div
+                    className="elementor-element elementor-element-e4e5f11 elementor-widget elementor-widget-spacer"
+                    data-id="e4e5f11"
+                    data-element_type="widget"
+                    data-widget_type="spacer.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-spacer">
+                        <div className="elementor-spacer-inner" />
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-b6a0bc4 elementor-widget elementor-widget-heading"
+                    data-id="b6a0bc4"
+                    data-element_type="widget"
+                    data-widget_type="heading.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <h2 className="elementor-heading-title elementor-size-default">
+                        Top Places <span className="text-theme">Listing</span>
+                      </h2>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-6eb8be3 elementor-widget elementor-widget-spacer"
+                    data-id="6eb8be3"
+                    data-element_type="widget"
+                    data-widget_type="spacer.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-spacer">
+                        <div className="elementor-spacer-inner" />
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="elementor-element elementor-element-d88c402 elementor-widget elementor-widget-text-editor"
+                    data-id="d88c402"
+                    data-element_type="widget"
+                    data-widget_type="text-editor.default"
+                  >
+                    <div className="elementor-widget-container">
+                      <div className="elementor-text-editor elementor-clearfix">
+                        <p>
+                          At vero eos et accusamus et iusto odio dignissimos
+                          ducimus qui blanditiis.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
